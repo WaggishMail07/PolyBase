ebadf9a13eddd4ef1586905bfaac2e6387ac78ca
WaggishMail07 <163008966+WaggishMail07@users.noreply.github.com>
2024-12-06 17:00:06 +0000

    Adding support for non base 10 input

--- main.py
+++ main.py
@@ -1,26 +1,80 @@
 import math
+letterDecNumValues = {
+    "1":1,
+    "2":2,
+    "3":3,
+    "4":4,
+    "5":5,
+    "6":6,
+    "7":7,
+    "8":8,
+    "9":9,
+    "a": 10,
+    "b": 11,
+    "c": 12,
+    "d": 13,
+    "e": 14,
+    "f": 15,
+    "e": 16,
+    "g": 17,
+    "h": 18,
+    "i": 19,
+    "j": 20,
+    "k": 21,
+    "l": 22,
+    "m": 23,
+    "n": 24,
+    "o": 25,
+    "p": 26,
+    "q": 27,
+    "r": 28,
+    "s": 29,
+    "t": 30,
+    "u": 31,
+    "v": 32,
+    "w": 33,
+    "x": 34,
+    "y": 35,
+    "z": 36
+}
 def frontPage():
     print("\n")
     print("hello world")
     print()
     print("Hello User, this is a program meant to translate Base 10 (decimal) integers into other bases")
     input("please press enter to continue")
-def defineBase(decNum):
+
+
+def defineBases(decNum):
+    base = 0
     base = int(input("Please enter a positive whole number that is less then 10 that will be the base that " + str(decNum) + " is translated to: "))
+    while(base==0 and 10 < base and base < 0):
+        base = int(input("please enter a positve whole number less then 10: "))
     return base
 
-def getDecNumToTranslate():
-    decNum = int(input("Please input a postive whole number to be translated from Base 10 to another base: "))
-    return decNum
+
+def getNumToTranslate():
+    inputNum = input("Please input a postive whole number to be translated from Base 10 to another base: ")
+    return inputNum
+
+
+def bigBaseToDec(INPUT, BASE): 
+        inputDec = INPUT
+        for i in range(0, len(INPUT)):
+            inputDec[i] = int(letterDecNumValues[str(INPUT[i])])
+            print(str(INPUT[i]) + " --> " + str(inputDec[i]))
+
 
 def sumDec(INPUT, BASE):
     print("INPUT: " + str(INPUT))
     output = 0
+
     for i in range(0, len(INPUT)):
         print("INPUT[i]*(BASE**i)=" + str(INPUT[i]*(BASE**i)) + "   | i: " + str(i))
         output+=INPUT[i]*(BASE**i)
     print("sumDec output: " + str(output))
     return output
+
 
 def translate(DECNUM, BASE):
     # this is the "highest power" or the max number of places/digits the final number can have
